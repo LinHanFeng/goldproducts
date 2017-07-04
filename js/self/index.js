ed0e568352d8e176672abf49dbd323cdc7bb9362
linhanfeng <396451011@qq.com>
2017-07-04 22:35:37 +0800

    add

--- js/self/index.js
+++ js/self/index.js
@@ -2,10 +2,26 @@ $(function(){
 	var index = {
 		init:function(){
 			this.oLoad();		//页面初始化
+			this.getBanner();		//banner获取
 			this.oSwiper();		//滑动
+			this.getNews();		//news获取
 		},
 		oLoad:function(){
+			
+		},
+		getBanner:function(){
 			var dataUrl = "http://www.coskobo.com/appserver/index.php/home/banner/bannerlistindex";
+			jsonData.getData(dataUrl,"GET",{},function(data){
+				console.log(data);
+				var oHtml = template("bannerTpl",data);
+				$(".m-index-banner").find(".swiper-wrapper").html(oHtml);
+			})
+			var mySwiper = new Swiper('.swiper-container-banner',{
+				direction : 'horizontal'
+			})
+		},
+		getNews:function(){
+			var dataUrl = "http://www.coskobo.com/appserver/index.php/home/news/newslistindex";
 			jsonData.getData(dataUrl,"GET",{},function(data){
 				console.log(data);
 			})
