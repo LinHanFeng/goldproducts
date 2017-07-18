c789d8201a513541d9c2c388544d896da31fc4bd
linhanfeng <396451011@qq.com>
2017-07-18 21:43:42 +0800

    分类页

--- build/js/self/classify.js
+++ build/js/self/classify.js
@@ -3,11 +3,27 @@
 var oDomain = "http://www.coskobo.com/appserver/index.php";
 var classify = {
 	init: function init() {
-		this.oLoad();
+		this.oLoad(); //页面初始化
+		this.getBasics(); //获取分类模块基本信息
+		this.getAllseal(); //获取所有印材
+		this.getGoodsList(); //获取分类商品
 	},
-	oLoad: function oLoad() {
+	oLoad: function oLoad() {},
+	getBasics: function getBasics() {
 		var dataUrl = oDomain + "/home/category/cateBaseInfo";
-		jsonData.getData(dataUrl, "GRT", {}, function (data) {
+		jsonData.getData(dataUrl, "GET", { "catId": "61" }, function (data) {
+			console.log(data);
+		});
+	},
+	getAllseal: function getAllseal() {
+		var dataUrl = oDomain + "/home/category/materialist";
+		jsonData.getData(dataUrl, "GET", { "catId": "61" }, function (data) {
+			console.log(data);
+		});
+	},
+	getGoodsList: function getGoodsList() {
+		var dataUrl = oDomain + "/home/category/cateGoodsList";
+		jsonData.getData(dataUrl, "GET", { "catId": "61" }, function (data) {
 			console.log(data);
 		});
 	}
--- js/self/classify.js
+++ js/self/classify.js
@@ -1,11 +1,29 @@
 const oDomain = "http://www.coskobo.com/appserver/index.php";
 const classify = {
 	init : function(){
-		this.oLoad();
+		this.oLoad();		//页面初始化
+		this.getBasics();	//获取分类模块基本信息
+		this.getAllseal();	//获取所有印材
+		this.getGoodsList();		//获取分类商品
 	},
 	oLoad : function(){
+
+	},
+	getBasics:function(){
 		let dataUrl = oDomain + "/home/category/cateBaseInfo";
-		jsonData.getData(dataUrl,"GRT",{},function(data){
+		jsonData.getData(dataUrl,"GET",{"catId":"61"},function(data){
+			console.log(data);
+		})
+	},
+	getAllseal:function(){
+		let dataUrl = oDomain + "/home/category/materialist";
+		jsonData.getData(dataUrl,"GET",{"catId":"61"},function(data){
+			console.log(data);
+		})
+	},
+	getGoodsList:function(){
+		let dataUrl = oDomain + "/home/category/cateGoodsList";
+		jsonData.getData(dataUrl,"GET",{"catId":"61"},function(data){
 			console.log(data);
 		})
 	}
