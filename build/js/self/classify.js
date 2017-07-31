6a6e3bc7159b900e6af79c6b7d71f856a3e552be
飞鸟 <feiniao@qiantanglicai.com>
2017-07-31 16:39:55 +0800

    购物车

--- build/js/self/classify.js
+++ build/js/self/classify.js
@@ -1,134 +1,136 @@
-"use strict";
-
-var oDomain = "http://www.coskobo.com/appserver/index.php";
-var catId = getQueryString("catId");
-var classify = {
-	init: function init() {
-		this.oLoad(); //页面初始化
-		this.getMenu(); //获取菜单列表	
+const oDomain = "http://www.coskobo.com/appserver/index.php";
+const catId = getQueryString("catId");
+const classify = {
+	init : function(){
+		this.oLoad();		//页面初始化
+		this.getMenu();		//获取菜单列表	
 		// this.oMenu();		//菜单列表操作PS:getMenu调用
-		this.getBasics(); //获取分类模块基本信息
-		this.getAllseal(); //获取所有印材
-		this.getGoodsList(); //获取分类商品
+		this.getBasics();	//获取分类模块基本信息
+		this.getAllseal();	//获取所有印材
+		this.getGoodsList();		//获取分类商品
 		//this.goInfo();			//跳转详情页PS:getGoodsList调用
 	},
-	oLoad: function oLoad() {
-		$(".m-common-menu").on("click", function () {
+	oLoad : function(){
+		$(".m-common-menu").on("click",function(){
 			$(".m-common-menu-box").show();
-		});
-		$(".m-detail-backbtn").on("click", function () {
+		})
+		$(".m-detail-backbtn").on("click",function(){
 			window.history.back();
-		});
+		})
 	},
-	getMenu: function getMenu() {
-		var dataUrl = oDomain + "/home/index/menuList";
-		jsonData.getData(dataUrl, "GET", {}, function (data) {
+	getMenu:function(){
+		let dataUrl = oDomain + "/home/index/menuList";
+		jsonData.getData(dataUrl,"GET",{},function(data){
 			console.log(data);
-			var oHtml = template("menuTpl", data);
+			let oHtml = template("menuTpl",data);
 			$(".m-common-menu-content-lists").html(oHtml);
-			if (data.data.other && data.data.other != "") {
-				for (var i = 0; i < data.data.other.length; i++) {
-					var _oHtml = '<div class="m-common-menu-content-list">' + '<a href="' + data.data.other[i].href + '">' + data.data.other[i].name;
-					'</a>' + '</div>';
+			if(data.data.other && data.data.other !=""){						
+				for(let i=0;i<data.data.other.length;i++){
+					let oHtml = '<div class="m-common-menu-content-list">'+
+					'<a href="'+data.data.other[i].href+'">'+data.data.other[i].name
+					'</a>'+
+					'</div>';
 					$(".m-common-menu-content-lists").append();
 				}
 			}
 			classify.oMenu();
-		});
-		$(".m-common-menu").on("click", function () {
+		})
+		$(".m-common-menu").on("click",function(){
 			$(".m-common-menu-box").show();
-		});
+		})
 	},
-	oMenu: function oMenu() {
-		$(".m-common-menu-content-list-header").each(function (index, elem) {
-			$(elem).on("click", function () {
+	oMenu:function(){
+		$(".m-common-menu-content-list-header").each(function(index,elem){
+			$(elem).on("click",function(){
 				$(elem).find(".jt img").toggleClass("fan");
 				$(elem).siblings("ul").toggle();
-			});
-		});
-		$(".m-common-menu-content-header").find(".btn").on("click", function () {
+			})
+		})
+		$(".m-common-menu-content-header").find(".btn").on("click",function(){
 			$(".m-common-menu-box").hide();
-		});
-		$(".m-common-menu-close").on("click", function () {
+		})
+		$(".m-common-menu-close").on("click",function(){
 			$(".m-common-menu-box").hide();
-		});
+		})
 	},
-	getBasics: function getBasics() {
-		var dataUrl = oDomain + "/home/category/cateBaseInfo";
-		var param = {
-			"catId": catId
-		};
-		jsonData.getData(dataUrl, "GET", { "data": JSON.stringify(param) }, function (data) {
+	getBasics:function(){
+		let dataUrl = oDomain + "/home/category/cateBaseInfo";
+		let param = {
+			"catId":catId
+		}
+		jsonData.getData(dataUrl,"GET",{"data":JSON.stringify(param)},function(data){
 			console.log(data);
 			$(".f-nowpage a").text(data.data.cat_name);
-			for (var v in data.data) {
+			for(let v in data.data){
 				console.log(v);
 				console.log(data.data[v]);
-				if (v != "cat_id" || v != "cat_name") {
+				if(v != "cat_id" || v != "cat_name"){
 					$(".m-classify-ranking").show();
 					$(".m-classify-ranking .content").append(data.data[v]);
 				}
 			}
-		});
+		})
 	},
-	getAllseal: function getAllseal() {
-		var dataUrl = oDomain + "/home/category/materiallist";
-		var param = {
-			"catId": catId
-		};
-		if (sessionStorage.materiallist) {
-			console.log(JSON.parse(sessionStorage.materiallist));
-			var data = JSON.parse(sessionStorage.materiallist);
-			for (var i = 0; i < data.data.length; i++) {
-				var oName = data.data[i].name;
-				if (oName.indexOf("<br />") > 0) {
+	getAllseal:function(){
+		let dataUrl = oDomain + "/home/category/materiallist";
+		let param = {
+			"catId":catId
+		}
+		if(sessionStorage.materiallist){
+			console.log(JSON.parse(sessionStorage.materiallist))
+			let data = JSON.parse(sessionStorage.materiallist);
+			for(let i=0;i<data.data.length;i++){
+				let oName = data.data[i].name;
+				if(oName.indexOf("<br />")>0){
 					data.data[i].name = oName.split("<br />");
 				}
 			}
-			var oHtml = template("classifyTpl", data);
+			let oHtml = template("classifyTpl",data);
 			$(".m-classify-type").find(".content").html(oHtml);
-			$(".m-classify-type .btn").on("click", function () {
+			$(".m-classify-type .btn").on("click",function(){
 				$(".m-classify-type ul").toggle();
-			});
-		} else {
-			jsonData.getData(dataUrl, "GET", { "data": JSON.stringify(param) }, function (data) {
-				if (data.code == 0) {
-					for (var _i = 0; _i < data.data.length; _i++) {
-						var _oName = data.data[_i].name;
-						if (_oName.indexOf("<br />") > 0) {
-							data.data[_i].name = _oName.split("<br />");
+			})
+		}else{
+			jsonData.getData(dataUrl,"GET",{"data":JSON.stringify(param)},function(data){
+				if(data.code == 0){
+					for(let i=0;i<data.data.length;i++){
+						let oName = data.data[i].name;
+						if(oName.indexOf("<br />")>0){
+							data.data[i].name = oName.split("<br />");
 						}
 					}
 					sessionStorage.materiallist = JSON.stringify(data);
-					var _oHtml2 = template("classifyTpl", data);
-					$(".m-classify-type").find(".content").html(_oHtml2);
-					$(".m-classify-type .btn").on("click", function () {
+					let oHtml = template("classifyTpl",data);
+					$(".m-classify-type").find(".content").html(oHtml);
+					$(".m-classify-type .btn").on("click",function(){
 						$(".m-classify-type ul").toggle();
-					});
+					})
 				}
-			});
+			})
 		}
+
+		
 	},
-	getGoodsList: function getGoodsList() {
-		var dataUrl = oDomain + "/home/category/cateGoodsList";
-		var param = {
-			"catId": catId
-		};
-		jsonData.getData(dataUrl, "GET", { "data": JSON.stringify(param) }, function (data) {
+	getGoodsList:function(){
+		let dataUrl = oDomain + "/home/category/cateGoodsList";
+		let param = {
+			"catId":catId
+		}
+		jsonData.getData(dataUrl,"GET",{"data":JSON.stringify(param)},function(data){
 			console.log(data);
-			if (data.code == 0) {
-				var oHtml = template("moduleTpl", data);
+			if(data.code == 0){
+				let oHtml = template("moduleTpl",data);
 				$(".m-classify-modules").html(oHtml);
 				classify.goInfo();
 			}
-		});
+		})
 	},
-	goInfo: function goInfo() {
-		$(".modules-list-content-scroll li").each(function (index, elem) {
-			$(elem).on("click", ".img,.detail", function () {
-				window.location.href = "detail.html?goodsId=" + $(elem).attr("data-id");
-			});
-		});
+	goInfo:function(){
+		$(".modules-list-content-scroll li").each(function(index,elem){
+			$(elem).on("click",".img,.detail",function(){
+				window.location.href = "detail.html?goodsId="+$(elem).attr("data-id");
+			})
+		})
 	}
-};
+}
 classify.init();
--- build/js/self/detail.js
+++ build/js/self/detail.js
@@ -1,59 +1,57 @@
-"use strict";
-
-var oDomain = "http://www.coskobo.com/appserver/index.php";
-var goodsId = getQueryString("goodsId");
-var detail = {
-	init: function init() {
-		this.oLoad(); //页面初始化
-		this.oMenu(); //菜单详情
-		this.getDetail(); //获取详情
+const oDomain = "http://www.coskobo.com/appserver/index.php";
+const goodsId = getQueryString("goodsId");
+const detail = {
+	init:function(){
+		this.oLoad();		//页面初始化
+		this.oMenu();		//菜单详情
+		this.getDetail();	//获取详情
 	},
-	oLoad: function oLoad() {
-		$(".m-common-menu").on("click", function () {
+	oLoad:function(){
+		$(".m-common-menu").on("click",function(){
 			$(".m-common-menu-box").show();
-		});
-		$(".m-detail-backbtn").on("click", function () {
+		})
+		$(".m-detail-backbtn").on("click",function(){
 			window.history.back();
-		});
+		})
 	},
-	oMenu: function oMenu() {
-		$(".m-common-menu-content-list-header").each(function (index, elem) {
-			$(elem).on("click", function () {
+	oMenu:function(){
+		$(".m-common-menu-content-list-header").each(function(index,elem){
+			$(elem).on("click",function(){
 				$(elem).siblings("ul").toggle();
-			});
-		});
-		$(".m-common-menu-content-header").find(".btn").on("click", function () {
+			})
+		})
+		$(".m-common-menu-content-header").find(".btn").on("click",function(){
 			$(".m-common-menu-box").hide();
-		});
-		$(".m-common-menu-close").on("click", function () {
+		})
+		$(".m-common-menu-close").on("click",function(){
 			$(".m-common-menu-box").hide();
-		});
+		})
 	},
-	getDetail: function getDetail() {
-		var dataUrl = oDomain + "/home/Goods/goodsInfo";
-		var param = {
-			"goodsId": goodsId
-		};
-		jsonData.getData(dataUrl, "GET", { "data": JSON.stringify(param) }, function (data) {
+	getDetail:function(){
+		let dataUrl = oDomain + "/home/Goods/goodsInfo";
+		let param = {
+			"goodsId":goodsId
+		}
+		jsonData.getData(dataUrl,"GET",{"data":JSON.stringify(param)},function(data){
 			console.log(data);
-			if (data.code == 0) {
+			if(data.code==0){
 				console.log(data.data);
-				$(".m-detail-product .pic img").attr({ "src": data.data.goods_img });
+				$(".m-detail-product .pic img").attr({"src":data.data.goods_img});
 				$(".m-detail-product .name").text(data.data.goods_name);
-				if (data.data.fast != "1") {
+				if(data.data.fast != "1"){
 					$(".f-fast").hide();
 				}
-				if (data.data.ten_years != "1") {
+				if(data.data.ten_years != "1"){
 					$(".f-fast").hide();
 				}
-				if (data.data.shadow != "1") {
+				if(data.data.shadow != "1"){
 					$(".f-fast").hide();
 				}
 				$(".m-detail-engravingins").html(data.data.goods_desc);
-			} else {
+			}else{
 				console.error("请求失败");
 			}
-		});
+		})
 	}
-};
+}
 detail.init();
